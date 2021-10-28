1d2b50e241a91cc0b7ee8d10884b970ffdbf369d
Fellous <fellousdavid95200@gmail.com>
2021-10-28 16:49:54 +0200

    lancement du projet

---
+++ vues/vAccueil.php
@@ -0,0 +1,10 @@
+  
+
+<div class="jumbotron masthead">
+  <div class="container">
+    <h2>voici le projet vincent </h2>
+    <img src="images/logoAcceuil.png" />
+    <hr>
+    <p>Envoyez des fleurs avec Lafleur.</p>
+  </div>
+</div>
